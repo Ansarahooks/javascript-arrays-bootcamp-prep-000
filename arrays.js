bdf2ca4c80ad441e2b1a92b51457efe011900352
ansara.hooks <ansara.hooks@gmail.com>
2018-10-25 20:08:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+function addElementToBeginningOfArray(array,element){
+  var myArray = [array];
+  myArray = [element, ...array];
+  return myArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array,element){
+  var myArray = [array];
+  myArray = [...array, element];
+  return myArray;
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array,index){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
